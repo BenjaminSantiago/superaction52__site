e4a8c4644ee97e158d07b50e063e395d71860efa
Benjamin Santiago <santiago.benjamin@gmail.com>
2026-01-10 14:18:13 -0500

    put the first rom in there

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
 export default function HOME()
 {
   return (
-    <main style={{padding: "2rem", fontFamily: "system-ui"}}>
+    <>
+    <main>
       <h1> SUPER ACTION 52</h1>
-      <p> One game per week! </p>
+      <p> One (small, Super Nintendo) game per week! </p>
+
+      <p> 
+        <a href = "https://benjaminsantiago.com">from Benjamin Santiago</a>
+      </p>
+
+      <p> 
+          <a href = "https://github.com/BenjaminSantiago/superaction52__games">
+            follow on github!
+          </a>
+      </p>
+
+      <h2> ROMS </h2>
+      <ul>
+        <li>
+          <a href = "ROMS/00__JUPITER-SCOPE-v01.smc">Week 1. Jupiter Scope v.0.1</a>
+        </li>
+      </ul>
+      
     </main>
+    </>
   );
 }
