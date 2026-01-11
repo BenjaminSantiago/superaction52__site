16dd63bfa374fd10d3f2a4679e5cf2af2ab008b4
Benjamin Santiago <santiago.benjamin@gmail.com>
2026-01-11 00:24:50 -0500

    small edits, adding video

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,10 @@ export default function HOME()
         </li>
       </ul>
       
+      <h2>VIDEOS/UPDATES</h2>
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/4nx-mF4-m6o?si=VdlAHwDOhl3Jw2TG" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
     </main>
     </>
   );
